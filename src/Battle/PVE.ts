839971fdb3be19d23050d8c0b0b1627771d131c7
cjheyde <carlaheyde@gmail.com>
2022-09-24 03:39:04 -0300

    feat: :white_check_mark: PVE class ok

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,37 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  private _charFighter: Fighter;
+  private _monsters: SimpleFighter[];
+
+  constructor(charFighter: Fighter, monsters: SimpleFighter[]) {
+    super(charFighter);
+    this._charFighter = charFighter;
+    this._monsters = monsters;
+  }
+
+  get charFighter(): Fighter {
+    return this._charFighter;
+  }
+
+  get monsters(): SimpleFighter[] {
+    return this._monsters;
+  }
+
+  // da o veredito da batalha: 1 if player wins, -1 otherwise
+  fight(): number {
+    this._monsters.map((opponent) => {
+      if (this.charFighter.lifePoints >= opponent.lifePoints) {
+        this.charFighter.attack(opponent);
+      }
+      opponent.attack(this.charFighter);
+      if (this.charFighter.lifePoints <= 0 || opponent.lifePoints <= 0) {
+        return super.fight();
+      }
+    });
+    return super.fight();
+  }
+}
+
+export default PVE;
